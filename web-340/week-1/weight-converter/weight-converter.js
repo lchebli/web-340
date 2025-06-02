37356f5749b00cd247616334f5fa2ec044ce9d8f
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-06-01 21:23:29 -0700

    Assignment-1.3

--- web-340/week-1/weight-converter/weight-converter.js
+++ web-340/week-1/weight-converter/weight-converter.js
@@ -1,10 +1,21 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Leslie Khattarchebli
+ * Date: 5/31/2025
+ * File Name: weight-converter
+ * Description: A simple weight converter
 */
 
 "use strict";
 
 // TODO: Implement the weight conversion logic here
+
+// Function to convert pounds to kilograms
+function poundsToKilograms(pounds) {
+  const conversionRate = 0.453592; // 1 pound = 0.453592 kilograms
+  return pounds * conversionRate;
+}
+
+// Example usage
+const pounds = 150; // Replace with your value
+const kilograms = poundsToKilograms(pounds);
+console.log(`${pounds} pounds is approximately ${kilograms.toFixed(2)} kilograms.`);
