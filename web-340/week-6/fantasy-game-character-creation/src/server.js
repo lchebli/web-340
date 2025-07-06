4520cd8ad70b12bb4ae5a81072fc000368bb3602
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-07-06 13:21:22 -0700

    assignment6-done

--- web-340/week-6/fantasy-game-character-creation/src/server.js
+++ web-340/week-6/fantasy-game-character-creation/src/server.js
@@ -1,10 +1,42 @@
+
 const http = require('http');
 const url = require('url');
 
-// TODO: Implement your server here
+const parsedUrl = url.parse(req.url, true); // `true` returns `query` as an object
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+
+let createdCharacter = null;
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const { pathname, searchParams } = url;
+
+  if (req.method === 'POST' && pathname === '/create-character') {
+    createdCharacter = {
+      class: searchParams.get('Warrior, Mage, Rouge'),
+      gender: searchParams.get('Male, Female, Other'),
+      funFact: searchParams.get('funFact'),
+    };
+    res.end('Character created successfully!');
+  
+  } else if (req.method === 'POST' && pathname === '/confirm-character') {
+    res.end('Character confirmed!');
+  
+  } else if (req.method === 'GET' && pathname === '/view-character') {
+    if (createdCharacter) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(createdCharacter));
+    } else {
+      res.end('No character created yet.');
+    }
+  
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+
 });
 
 server.listen(3000, () => {
--- web-340/week-6/fantasy-game-character-creation/test/server.spec.js
+++ web-340/week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,36 @@
 const http = require('http');
 const server = require('../src/server');
+const request = require('supertest');
 
-// TODO: Implement your tests here
+"scripts"; {
+  "start"; "node src/server.js",
+  "test"; "jest"
+}
+
+//unit tests
+
+  test('POST /create-character creates a new character', async () => {
+    const response = await request(server)
+      .post('/create-character?class=Rogue&gender=Female&funFact=Loves%20shiny%20things');
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toMatch(/character created/i);
+  });
+
+  test('POST /confirm-character confirms character creation', async () => {
+    const response = await request(server).post('/confirm-character');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toMatch(/confirmed/i);
+  });
+
+  test('GET /view-character returns the created character', async () => {
+    const response = await request(server).get('/view-character');
+
+    expect(response.statusCode).toBe(200);
+    const character = JSON.parse(response.text);
+
+    expect(character).toHaveProperty('class', 'Rogue');
+    expect(character).toHaveProperty('gender', 'Female');
+    expect(character).toHaveProperty('funFact', 'Loves shiny things');
+  });
